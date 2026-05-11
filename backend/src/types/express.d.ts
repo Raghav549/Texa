fbb2ddf509f26548c8d5e5f7d6d55b9a6c5784cd
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-11 19:19:43 +0530

    Extend Express Request interface with userId and role

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,19 @@
+import { Role } from '@prisma/client';
+
+declare global {
+
+  namespace Express {
+
+    interface Request {
+
+      userId?: string;
+
+      role?: Role;
+
+    }
+
+  }
+
+}
+
+export {};
