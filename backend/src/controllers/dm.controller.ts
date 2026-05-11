47d4d372afd1171b21c6030381c2a42a25d69558
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-11 11:26:23 +0530

    Delete backend/src/controllers/dm.controller.ts

--- backend/src/controllers/dm.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { prisma } from '../config/db';
-export const sendMessage = async (req: Request, res: Response) => {
-  const { receiverId, content, mediaUrl } = req.body;
-  const msg = await prisma.message.create({ data: { senderId: req.userId!, receiverId, content, mediaUrl } });
-  res.json(msg);
-};
