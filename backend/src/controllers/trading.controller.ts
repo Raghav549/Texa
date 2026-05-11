f48fe63394ddcdcadf59843935dbd5b05a6baacd
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-11 11:24:17 +0530

    Delete backend/src/controllers/trading.controller.ts

--- backend/src/controllers/trading.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import { prisma } from '../config/db';
-export const getActiveTrade = async (_req: Request, res: Response) => {
-  const trade = await prisma.tradeDay.findFirst({ where: { isActive: true }, orderBy: { date: 'desc' } });
-  res.json(trade);
-};
