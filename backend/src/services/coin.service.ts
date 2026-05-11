178f5d68e21dd9bc3a61df08adf4e7a018feeaaf
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-11 16:43:37 +0530

    Delete backend/src/services/coin.service.ts

--- backend/src/services/coin.service.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '../config/db';
-
-export async function updateCoins(userId: string, delta: number) {
-  return await prisma.user.update({
-    where: { id: userId },
-     { coins: { increment: delta } }
-  });
-}
