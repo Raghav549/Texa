c5aa87c2a06051e7d18c21164e4059bb0047ab67
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-11 11:25:32 +0530

    Delete backend/src/controllers/room.controller.ts

--- backend/src/controllers/room.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { prisma } from '../config/db';
-export const createRoom = async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const room = await prisma.voiceRoom.create({  { title, hostId: req.userId! } });
-  res.json(room);
-};
