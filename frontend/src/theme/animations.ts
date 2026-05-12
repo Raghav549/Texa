12fbfcb5adc0c042df37fc692cb1b7416faeab07
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-12 13:06:17 +0530

    Delete frontend/src/theme/animations.ts

--- frontend/src/theme/animations.ts
+++
@@ -1,4 +0,0 @@
-import { Animated, Easing } from 'react-native';
-export const fadeIn = (ref: any, duration = 300) => Animated.timing(ref, { toValue: 1, duration, easing: Easing.out(Easing.ease), useNativeDriver: true }).start();
-export const scaleUp = (ref: any) => Animated.sequence([Animated.timing(ref, { toValue: 1.1, duration: 150, useNativeDriver: true }), Animated.timing(ref, { toValue: 1, duration: 150, useNativeDriver: true })]).start();
-export const slideUp = (ref: any) => Animated.spring(ref, { toValue: 1, tension: 40, friction: 10, useNativeDriver: true }).start();
